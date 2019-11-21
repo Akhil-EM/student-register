873504ce3d1712581a8edb0956d30962c2e5d2d4
akhil  em <akhilemcalicut@gmail.com>
2019-11-21 17:06:36 +0530

    changed port to defualt port

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.set("views","./src/views");
 app.set("view engine","ejs");
 
-// app.listen(process.env.PORT||4000,()=>{
-//     console.log(chalk.blueBright("port  is now active"));
-// });
-
-app.listen(4000,()=>{
+app.listen(process.env.PORT||4000,()=>{
     console.log(chalk.blueBright("port  is now active"));
 });
+
+// app.listen(4000,()=>{
+//     console.log(chalk.blueBright("port  is now active"));
+// });
